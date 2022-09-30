b9f3f5966d57c959775d279749ced463c95b5b17
Pavel Volchenko <2793553@gmail.com>
2022-09-30 15:28:33 +0300

    Init

---
+++ Task025/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Задача 25: Напишите цикл, который принимает на вход два числа 
+    (A и B) и возводит число A в натуральную степень B.
+    3, 5 -> 243 (3⁵)
+    2, 4 -> 16                                                  */
+
+int exponentiationUserNumbers(int firstUserNumber, int secondUserNumber)
+{
+    int result = firstUserNumber;
+    for (int i = 1; i < secondUserNumber; i++)
+    {
+        result = result * firstUserNumber;
+    }
+    return result;
+}
+
+Console.WriteLine("Для возведения в степень, введите два целых положительных числа.");
+Console.Write("Введите первое число: ");
+int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = exponentiationUserNumbers(firstUserNumber, secondUserNumber);
+Console.WriteLine($"Число {firstUserNumber} в степени {secondUserNumber} = {result}");
---
+++ Task027/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Задача 27: Напишите программу, которая принимает
+    на вход число и выдаёт сумму цифр в числе.
+    452 -> 11
+    82 -> 10
+    9012 -> 12                                     */
+
+int sumOfDigits(int userNumber)
+{
+    int result = 0;
+    while (userNumber > 0)
+    {
+        result += userNumber % 10;
+        userNumber /= 10;
+    }
+    return result;
+}
+
+Console.Write("Введите целое положительное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = sumOfDigits(userNumber);
+Console.WriteLine($"Сумма цифр = {result}");
